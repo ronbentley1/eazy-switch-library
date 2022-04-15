4bec3cb9742bc1e47f511fd2f7860182c42b0220
Ron Bentley <ron.bentley1@ntlworld.com>
2022-04-15 13:56:04 +0100

    Add files via upload

---
+++ src/ez_switch_lib.cpp
@@ -0,0 +1,293 @@
+// Arduino Switch Library for configuring different switch type wired
+// in common circuit schemes.
+//
+// Ron Bentley, Stafford (UK), March 2021, version 2.0
+//
+// This example and code is in the public domain and
+// may be used without restriction and without warranty.
+//
+
+#include <Arduino.h>
+#include <ez_switch_lib.h>
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Set up switch cotrol structure and initialise internal variables.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Switches::Switches(byte max_switches)
+{
+  // Establish the switch control structure (switches) of the size required.
+  //
+  switches = (switch_control *)malloc(sizeof(*switches) * max_switches);
+  if (switches == NULL) {
+    // malloc failure
+    Serial.begin(115200);
+    Serial.println("!!Failure to acquire memory of required size - PROGRAM TERMINATED!!");
+    Serial.flush();
+    exit(1);
+  }
+
+  // Initialise private variables
+  _num_entries  = 0;            // will be incremented each time a switch is added, up to _max_switches
+  _max_switches = max_switches; // transfer to internal variable
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Generic switch read function.
+// Read the switch defined by the function parameter.
+// Function returns a value indicating if the switch
+// has undergone a transition or not.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bool Switches::read_switch(byte sw) {
+  bool sw_status;
+  if (sw < 0 || sw >= _num_entries) return !switched; // out of range, slot 'sw' is not configured with a switch
+  if (switches[sw].switch_type == button_switch) {
+    sw_status = read_button_switch(sw);
+  } else {
+    sw_status = read_toggle_switch(sw);
+  }
+  // now determine if switch has output pin associated and if switched
+  // flip the output's status, ie HIGH->LOW, or LOW->HIGH
+  if (sw_status == switched && switches[sw].switch_out_pin > 0)
+  {
+    // flip the output level of associated switch output pin, if defined
+    bool status = switches[sw].switch_out_pin_status; // last status value of out_pin
+    status = HIGH - status;                           // flip the status value
+    switches[sw].switch_out_pin_status = status;      // update current status value
+    digitalWrite(switches[sw].switch_out_pin, status);// change status of linked pin
+  }
+  return sw_status;
+}  // End of read_switch
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Generic toggle switch read function.
+// Test the toggle switch to see if its status has changed since last look.
+// Note that although switch status is a returned value from the function,
+// the current status of the switch ('switches[sw].switch_status') is always
+// maintained and can be tested outside of the function at any point/time.
+// It will either have a status of 'on' or '!on' (ie off).
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bool Switches::read_toggle_switch(byte sw) {
+  byte switch_pin_reading = digitalRead(switches[sw].switch_pin);  // test current state of toggle pin
+  if (switches[sw].switch_circuit_type == circuit_C2) {
+    // Need to invert HIGH/LOW if circuit design sets
+    // pin HIGH representing switch in off state.
+    // ie inititialised as INPUT_PULLUP
+    switch_pin_reading = !switch_pin_reading;
+  }
+  if (switch_pin_reading != switches[sw].switch_status && !switches[sw].switch_pending) {
+    // Switch change detected so start debounce cycle
+    switches[sw].switch_pending = true;
+    switches[sw].switch_db_start = millis();  // set start of debounce timing
+  }
+  if (switches[sw].switch_pending) {
+    // We are in the switch transition cycle so check if debounce period has elapsed
+    if (millis() - switches[sw].switch_db_start >= _debounce) {
+      // Debounce period elapsed so assume switch has settled down after transition
+      switches[sw].switch_status  = !switches[sw].switch_status;  // flip status
+      switches[sw].switch_pending = false;                        // cease transition cycle
+      last_switched_id = sw;   // indicates the last switch to have been processed by read function
+      return switched;
+    }
+  }
+  return !switched;
+} // End of read_toggle_switch
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Read button switch function.
+// Generic button switch read function.
+// Reading is controlled by:
+//   a. the function parameter which indicates which switch
+//      is to be polled, and
+//   b. the switch control struct(ure), referenced by a).
+//
+// Note that this function works in a nonexclusive way
+// and incorporates debounce code.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bool Switches::read_button_switch(byte sw) {
+  byte switch_pin_reading = digitalRead(switches[sw].switch_pin);
+  if (switch_pin_reading == switches[sw].switch_on_value) {
+    // Switch is pressed (ON), so start/restart debounce process
+    switches[sw].switch_pending = true;
+    switches[sw].switch_db_start   = millis();  // start elapse timing
+    return !switched;                           // now waiting for debounce to conclude
+  }
+  if (switches[sw].switch_pending && switch_pin_reading != switches[sw].switch_on_value) {
+    // Switch was pressed, now released (OFF), so check if debounce time elapsed
+    if (millis() - switches[sw].switch_db_start >= _debounce) {
+      // debounce time elapsed, so switch press cycle complete
+      switches[sw].switch_pending = false;
+      last_switched_id = sw;   // indicates the last switch to have been processed by read function
+      return switched;
+    }
+  }
+  return !switched;
+}  // End of read_button_switch
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Add given switch to switch control structure, but validate
+// given paramters and ensure there is a free slot.
+//
+// Return values from add_switch are:
+//
+//    >= 0 the switch control structure entry number ('switch_id')
+//         for the switch added,
+//      -1 add_falure - no slots available in the switch
+//         control structure,
+//      -2 bad_params - given paramter(s) for switch are
+//         not valid.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+int Switches::add_switch(byte sw_type, byte sw_pin, byte circ_type) {
+  if ((sw_type != button_switch && sw_type != toggle_switch) ||
+      (circ_type != circuit_C1 &&
+       circ_type != circuit_C2 &&
+       circ_type != circuit_C3)) return bad_params;  // bad paramters
+  if (_num_entries < _max_switches) {
+    // room to add another switch...initialise the switch's
+    // data depending on type of switch and circuit
+    switches[_num_entries].switch_type    = sw_type;
+    switches[_num_entries].switch_pin     = sw_pin;
+    switches[_num_entries].switch_circuit_type   = circ_type;
+    switches[_num_entries].switch_pending = false;
+    switches[_num_entries].switch_db_start   = 0;
+    // define what on means for this switch:
+    if (circ_type != circuit_C2) {
+      // circuit_C1 and circuit_C3 are used with pull down resistors
+      // if circuit_C1 (INPUT) then and external 10k ohm resistor needs to be fitted
+      // to the switch circuit, but if circuit_C3 (INPUT_PULLDOWN) then the resistor
+      // is internal to the microcontroller.
+      // Note that the 'on' state is represented by HIGH (3.3/5v)
+      switches[_num_entries].switch_on_value = HIGH;
+    } else {
+      // circuit_C2 (INPUT_PULLUP) with internal microcontroller pull up resistor
+      // Note that the 'on' state is represented by LOW (0v)
+      switches[_num_entries].switch_on_value = LOW;
+    }
+    if (sw_type == button_switch) {
+      switches[_num_entries].switch_status = not_used;
+    } else {
+      switches[_num_entries].switch_status = !on;
+    }
+    pinMode(sw_pin, circ_type);  // establish pin set up
+    // ensure no mapping to an output pin until created explicitly
+    switches[_num_entries].switch_out_pin        = 0;
+    switches[_num_entries].switch_out_pin_status = LOW;  // set LOW unless explicitly changed
+
+    _num_entries++;              // point to next free slot
+    return _num_entries - 1;     // return 'switch_id' - given switch now added to switch control structure
+  } else return add_failure;     // no room left to add another switch!
+}  // End add_switch
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Link or delink the given switch to the given digital pin as an output
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+int Switches::link_switch_to_output(byte switch_id, byte output_pin, bool HorL) {
+  if (switch_id > _num_entries) {
+    return link_failure; // no such switch
+  }
+  if (output_pin == 0) {
+    // delink this output from this switch, set the output to the required level first
+    if (switches[switch_id].switch_out_pin == 0) {
+      // no output pin previously defined
+      return link_failure;
+    }
+    // set existing pin to level required state before clearing the link
+    digitalWrite(switches[switch_id].switch_out_pin, HorL);
+  } else {
+    // initialise given output pin
+    pinMode(output_pin, OUTPUT);
+    digitalWrite(output_pin, HorL); // set to param value until switched
+  }
+  switches[switch_id].switch_out_pin        = output_pin;
+  switches[switch_id].switch_out_pin_status = HorL;
+  return link_success;           // success
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Return the number of slots left unused
+// in the switch control structure.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+int Switches::num_free_switch_slots() {
+  return _max_switches - _num_entries;
+} // End num_free_switch_slots
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Set debounce period (milliseconds).
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void Switches::set_debounce(int period) {
+  if (period >= 0)  _debounce = period;
+}  // End set_debounce
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Print given switch control data.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void Switches::print_switch(byte sw) {
+  if (0 <= sw && sw < _num_entries ) {
+    Serial.print(F("sw_id:   = "));
+    Serial.println(sw);
+    Serial.print(F("sw_type  = "));
+    uint8_t sw_type = switches[sw].switch_type;
+    if (sw_type == button_switch)Serial.print(F("BUTTON SWITCH"));
+    else Serial.print(F("TOGGLE SWITCH"));
+    Serial.print(F("  sw_pin  = "));
+    Serial.print(switches[sw].switch_pin);
+    Serial.print(F("\tcirc_type = "));
+    uint8_t circ_type = switches[sw].switch_circuit_type;
+    if (circ_type == circuit_C1) {
+      Serial.print(F("INPUT/circuit_C1/"));
+    } else if (circ_type == circuit_C2) {
+      Serial.print(F("INPUT_PULLUP/circuit_C2/"));
+    } else {
+      Serial.print(F("INPUT_PULLDOWN/circuit_C3/"));
+    }
+    Serial.println(circ_type);
+    Serial.print("on_value = ");
+    if (switches[sw].switch_on_value == HIGH)Serial.print(F("HIGH"));
+    else Serial.print(F("LOW "));
+    Serial.print(F("\t\tsw_status = "));
+    uint8_t sw_status = switches[sw].switch_status;
+    if (sw_type == button_switch)Serial.print(F("n/a")); else {
+      if (sw_status == on)Serial.print(F("ON"));
+      else Serial.print(F("OFF"));
+    }
+    Serial.print(F("\tpending   = "));
+    if (switches[sw].switch_pending)Serial.print(F("YES"));
+    else Serial.print(F("NO "));
+    Serial.print(F("\tdb_start = "));
+    Serial.print(switches[sw].switch_db_start);
+    Serial.println(F(" msecs"));
+    if (switches[sw].switch_out_pin == 0) {
+      // switch does not have a linked output
+      Serial.println(F("*** No linked output pin"));
+    } else {
+      // switch has a linked output
+      Serial.print(F("Linked output pin = "));
+      Serial.print(switches[sw].switch_out_pin);
+      Serial.print(F("\tlinked pin status = "));
+      if (switches[sw].switch_out_pin_status == LOW)Serial.println(F("LOW"));
+      else Serial.println(F("HIGH"));
+    }
+    Serial.println();
+    Serial.flush();
+  }
+} // End print_switch
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Print all switch control data set up.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void Switches::print_switches() {
+  Serial.println(F("\nDeclared & configured switches:"));
+  for (byte sw = 0; sw < _num_entries; sw++) {
+    print_switch(sw);
+  }
+} // End print_switches
---
+++ src/ez_switch_lib.h
@@ -0,0 +1,72 @@
+// Arduino Switch Library for configuring different switch type wired
+// in common circuit schemes.
+//
+// Ron Bentley, Stafford (UK), March 2021, version 2.0
+//
+// This example and code is in the public domain and
+// may be used without restriction and without warranty.
+//
+
+#ifndef ez_switches_h
+#define ez_switches_h
+
+#include <Arduino.h>
+
+class Switches
+{
+  public:
+    Switches(byte max_switches);
+
+#define button_switch         1      // differentiates switch type
+#define toggle_switch         2      // ditto
+
+#define INPUT_PULLDOWN        9      // not valid for Arduino boards, but it is for ESP32 boards
+#define circuit_C1        INPUT      // switch circuit requires an external pull down 10k ohm resistor
+#define circuit_C2  INPUT_PULLUP     // switch circuit requires no other components beyond the switch
+#define circuit_C3  INPUT_PULLDOWN   // switch circuit requires no other components beyond the switch
+
+#define switched           true      // signifies switch has been pressed/switch cycle complete
+#define on                 true      // used for toggle switch status
+#define not_used           true      // helps self document code
+#define bad_params           -2      // invalid add_swith paramters
+#define add_failure          -1      // add_swith could not insert a given switch, ie no space left
+#define link_success          0      // output successfully linked to a switch
+#define link_failure         -1      // output pin could not be linked to a switch
+#define none_switched      255      // 'last_switched_id' initialised to this value
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // %                   Switch Control Sruct(ure) Declaration                 %
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    //
+    struct switch_control {
+      byte switch_type;                 // type of switch connected
+      byte switch_pin;                  // digital input pin assigned to the switch
+      byte switch_circuit_type;         // the type of circuit wired to the switch
+      bool switch_on_value;             // used for BUTTON SWITCHES only - defines what "on" means
+      bool switch_pending;              // records if switch in transition or not
+      long unsigned int switch_db_start;// records debounce start time when associated switch starts transition
+      bool switch_status;               // used for TOGGLE SWITCHES only - current state of toggle switch.
+      byte switch_out_pin;              // the digital pin mapped to this switch, if any
+      bool switch_out_pin_status;       // the status of the mapped pin
+    } volatile *switches;               // memory will be created when class is initiated
+
+    volatile byte last_switched_id = none_switched;
+
+    // Functions available to end users
+    bool read_switch           (byte sw);
+    bool read_toggle_switch    (byte sw);
+    bool read_button_switch    (byte sw);
+    int  add_switch            (byte sw_type, byte sw_pin, byte circ_type);
+    int  link_switch_to_output (byte switch_id, byte output_pin, bool HorL);
+    int  num_free_switch_slots ();
+    void set_debounce          (int period);
+    void print_switch          (byte sw);
+    void print_switches        ();
+
+  private:
+    byte _num_entries  = 0;  // used for adding switches to switch control structure/list
+    byte _max_switches = 0;  // max switches user has initialise
+    int _debounce      = 10; // 10 millisecs if not specified by user code
+};
+
+#endif
